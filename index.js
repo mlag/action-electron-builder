4497384967bc2cebf2da1f35ba5e8cb47009c902
moritz <moritz.lage@gmail.com>
2021-04-13 13:08:58 +0200

    v1.7.2

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ const runAction = () => {
 	for (let i = 0; i < maxAttempts; i += 1) {
 		try {
 			run(
-				`npx electron-forge publish`,
+				`npm run publish`,
 				// `${useNpm ? "npx --no-install" : "yarn run"} ${cmd} --${platform} ${
 				// 	release ? "--publish always" : ""
 				// } ${args}`,
